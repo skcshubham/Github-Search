03a9bd99ab2dfb1709ef924009c0838d960b3554
Shubham Choudhary <work.shubhamchoudhary@gmail.com>
2021-04-05 16:45:09 +0530

    added user details page route

--- src/components/UserData/UserData.js
+++ src/components/UserData/UserData.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Spinner from "../Spinner/Spinner";
+import { Link } from "react-router-dom";
 
 class UserData extends Component {
 	componentDidMount() {
@@ -12,13 +13,13 @@ class UserData extends Component {
 			name,
 			avatar_url,
 			location,
+			company,
 			bio,
-			blog,
 			login,
 			html_url,
 			followers,
 			following,
-			public_reps,
+			public_repos,
 			public_gists,
 			hireable,
 		} = this.props.user;
@@ -28,9 +29,81 @@ class UserData extends Component {
 			return <Spinner />;
 		} else {
 			return (
-				<div>
-					<h1>{name}</h1>
-				</div>
+				<React.Fragment>
+					<Link to="/" className="btn btn-light">
+						back to home
+					</Link>
+					Open to opportunities: {""}
+					{hireable ? (
+						<i className="fas fa-check text-success" />
+					) : (
+						<i className="fas fa-times-circle text-danger" />
+					)}
+					<card className="card grid-2">
+						<div className="all-center">
+							<img
+								src={avatar_url}
+								className="round-img"
+								alt={name}
+								style={{ width: "180px" }}
+							/>
+							<h1>{name}</h1>
+							<p>Location : {location === null ? "Not Provided" : location}</p>
+						</div>
+						<div>
+							<h3>Bio:</h3>
+							{bio === null ? (
+								"Not Provided"
+							) : (
+								<React.Fragment>
+									<p>{bio}</p>
+								</React.Fragment>
+							)}
+							<a
+								href={html_url}
+								className="btn btn-dark m-1"
+								target="_blank"
+								rel="noreferrer"
+							>
+								visit {name}'s github
+							</a>
+							<li>
+								<strong>USERNAME : </strong>
+								{login !== null ? (
+									<React.Fragment>
+										<strong>{login}</strong>
+									</React.Fragment>
+								) : (
+									"No username"
+								)}
+							</li>
+							<li>
+								<strong>COMPANY : </strong>
+								{company !== null ? (
+									<React.Fragment>
+										<strong>{company}</strong>
+									</React.Fragment>
+								) : (
+									"doesn't work at any company"
+								)}
+							</li>
+						</div>
+					</card>
+					<div className="card text-center">
+						<badge className="badge badge-primary">
+							Followers : {followers}
+						</badge>
+						<badge className="badge badge-primary">
+							Following : {following}
+						</badge>
+						<badge className="badge badge-primary">
+							Public repos : {public_repos}
+						</badge>
+						<badge className="badge badge-primary">
+							Public gists : {public_gists}
+						</badge>
+					</div>
+				</React.Fragment>
 			);
 		}
 	}
