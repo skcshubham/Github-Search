979414fb9f617b17a5e4b726e5936fa04b994105
Shubham Choudhary <work.shubhamchoudhary@gmail.com>
2021-04-07 15:13:46 +0530

    fixed a typo

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,14 +8,15 @@ const About = () => {
 			<p>
 				A web app that lets users search their Github profile and check their
 				entire profile data as well as their top 5 recent repositories and its
-				details.
+				details such as the number of stars, forks and watchers that the
+				repository currently has.
 			</p>
 			<br />
 			<p>
 				This webapp is built with Github API for fetching user details and the
 				repository details of the user. If you want to contribute to this
-				project or suggest me some feature, you want find the link to the
-				repository here =>
+				project, feel free to raise a pull request or if you want to suggest me
+				some features, you can add your issues to the repository here.
 				<a
 					href="https://github.com/skcshubham/Github-Search"
 					target="_blank"
