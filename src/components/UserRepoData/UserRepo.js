7760fcb9dc4dc4ab4445eeb116c9e9c938e189a3
Shubham Choudhary <work.shubhamchoudhary@gmail.com>
2021-04-17 10:44:29 +0530

    added repository headline

--- src/components/UserRepoData/UserRepo.js
+++ src/components/UserRepoData/UserRepo.js
@@ -4,6 +4,7 @@ import UserRepoList from "./UserRepoList";
 const UserRepo = ({ repos }) => {
 	return (
 		<div>
+			{repos.length === 0 ? "" : <h1>Recent 5 Repositories : </h1>}
 			{repos.map((repo) => (
 				<UserRepoList key={repo.id} repo={repo} />
 			))}
